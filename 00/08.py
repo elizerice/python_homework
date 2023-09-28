fa9c17c08e03fcabe55c64c55f477fcf8d245b83
elizerice <114720693+elizerice@users.noreply.github.com>
2023-09-28 22:37:52 +0500

    Create 08.py

---
+++ 00/08.py
@@ -0,0 +1,7 @@
+first_name = input("Введите ваше имя ")
+second_name = input("Вашу фамилию")
+favourite_animal = input("Ваше любимое животное")
+zodiak = input("Ваш знак зодиака")
+print("Индивидуальный гороскоп для пользователя", first_name, second_name,
+"Кем вы были в прошлой жизни:", favourite_animal,
+". Ваш знак зодиака -", zodiak, ", поэтому вы - тонко чувствующая натура.")
