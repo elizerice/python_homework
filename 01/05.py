519180fe57414724f766c696f6d19bbf91be2fd0
elizerice <114720693+elizerice@users.noreply.github.com>
2023-10-03 18:54:11 +0500

    Create 05.py

---
+++ 01/05.py
@@ -0,0 +1,8 @@
+line_1 = input('введите 1 строку')
+line_2 = input('введите 2 строку')
+line_3 = input('введите 3 строку')
+if line_1 =='раз' or line_1 == 'один' and line_2 == 'два' and line_3 == 'три' or line_1 =='1' and line_2 == '2' and line_3 == '3':
+    print('ГОРИ')
+else:
+    print('НЕ ГОРИ')
+    
