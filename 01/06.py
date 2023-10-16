ae0d5fe4d1b7f335653e5db7d2ee6fa04c9e69dc
elizerice <114720693+elizerice@users.noreply.github.com>
2023-10-16 16:51:29 +0500

    Update 06.py

--- 01/06.py
+++ 01/06.py
@@ -1,7 +1,12 @@
 answer_1 = input('Любите ли вы котиков?')
 answer_2 = input('«Умеете ли вы программировать?')
-if line_1 !='раз' or line_2 != 'два':
-    print('ГОРИ')
+if answer_1 == 'да' and answer_2 == 'да':
+    print('У вас наверняка есть кот и ноутбук/пк!!!')
+elif answer_1 == 'да' and answer_2 == 'нет':
+    print('У вас лапки!')
+elif answer_1 == 'нет' and answer_2 == 'да':
+    print('Возможно, кошки вам чем-то не угодили..')
+elif answer_1 == 'нет' and answer_2 == 'нет':
+    print('Вы не программист и у вас нет кота')
 else:
-    print('НЕ ГОРИ')
-    
+    print('Ошибка!')
