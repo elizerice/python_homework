886308a3767c336a401c07b6b98c02a3fdb1a489
elizerice <114720693+elizerice@users.noreply.github.com>
2023-10-22 23:01:44 +0500

    Create 01.py

---
+++ 05/01.py
@@ -0,0 +1,11 @@
+num_1 = input()
+num_2 = input()
+for num in range(num_1, num_2):
+    if num // 3 == 0 and num // 5 == 0:
+        print('fizzbuzz')
+    elif num // 3 = 0:
+        print('fizz')
+    elif num // 5 = 0:
+        print('buzz')
+    else:
+        print(num)
