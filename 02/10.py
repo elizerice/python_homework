a5c8ff7072b98bdf0f1d0dbf032b207a6b17bea6
elizerice <114720693+elizerice@users.noreply.github.com>
2023-10-22 18:47:04 +0500

    Create 10.py

---
+++ 02/10.py
@@ -0,0 +1,13 @@
+num_1 = float(input())
+num_2 = float(input())
+action = input()
+if action == '+':
+    print(num_1 + num_2)
+elif action == '-':
+    print(num_1 - num_2)
+elif action == '*':
+    print(num_1 * num_2)
+elif action == '/':
+    print(num_1 / num_2)
+else:
+    print('888888')
