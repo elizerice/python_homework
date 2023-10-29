a4c8c77c63a27b39bfca0aa797f27df1547ec779
elizerice <114720693+elizerice@users.noreply.github.com>
2023-10-29 19:56:34 +0500

    Update 01.py

--- 05/01.py
+++ 05/01.py
@@ -1,11 +1,11 @@
-num_1 = input()
-num_2 = input()
-for num in range(num_1, num_2):
-    if num // 3 == 0 and num // 5 == 0:
+num_1 = int(input())
+num_2 = int(input())
+for num in range(num_1, num_2 + 1):
+    if num % 3 == 0 and num % 5 == 0:
         print('fizzbuzz')
-    elif num // 3 = 0:
+    elif num % 3 == 0:
         print('fizz')
-    elif num // 5 = 0:
+    elif num % 5 == 0:
         print('buzz')
     else:
         print(num)
