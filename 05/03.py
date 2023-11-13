4adf5e6786193a822aced377de892a81169b5f46
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-13 09:18:39 +0500

    Create 03.py

---
+++ 05/03.py
@@ -0,0 +1,14 @@
+num = 0
+cat = 0
+word = str()
+while word != 'стоп':
+    word = input()
+    num += 1
+    if cat == 0:
+        if 'кот' in word:
+            cat = 1
+            cat_line = num
+if cat == 0:
+    print(-1)
+else:
+    print(cat_line)
