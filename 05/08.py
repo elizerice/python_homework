f1543b0a98dd3852b005d92e7e96f0af7a9de8b5
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-17 22:15:14 +0500

    Update 08.py

--- 05/08.py
+++ 05/08.py
@@ -1,17 +1,19 @@
 num = 0
 cat = 0
 word = str()
-nums = str()
+cat_line = int()
+count = int()
 while word != 'стоп':
     word = input()
     num += 1
+    if cat == 0:
+        cat_line = num
     if 'кот' in word:
         cat = 1
-        nums += str(num)
-    
+        count += 1
     if word == 'стоп':
         break
 if cat == 0:
     print(-1)
 else:
-    print(nums)
+    print(count, cat_line)
