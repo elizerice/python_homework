2da64f45ec0585f1d05c4ae2c0987e246609da88
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-20 08:46:11 +0500

    Update 01.py

--- 06/01.py
+++ 06/01.py
@@ -8,4 +8,7 @@ num = input()
 while num != '':
     list_2.add(str(num))
     num = input()
-print(list_1 & list_2)
+if len(list_1 & list_2) != 0:
+    print(list_1 & list_2)
+else:
+    print('EMPTY')
