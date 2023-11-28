418bf3c212e07a68e27e6194bf60be6d168fc977
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-28 23:02:16 +0500

    Update 01.py

--- 10/01.py
+++ 10/01.py
@@ -1,1 +1,14 @@
-
+list = []
+yes_no = bool()
+for i in range(int(input())):
+    number = input()
+    list.append(number)
+num = int(input())
+for i in list:
+    for j in list:
+        if int(i) * int(j) == num:
+            yes_no = True
+if yes_no == True:
+    print('да')
+else:
+    print('нет')
