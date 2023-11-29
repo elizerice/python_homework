cb1f346ca514cb57c0f308fe3be6d3c555ca7f0e
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-29 21:01:43 +0500

    Update 01.py

--- 11/01.py
+++ 11/01.py
@@ -1,1 +1,10 @@
-
+line = input()
+line = line.split(' ')
+_list = []
+count = 0
+print(line)
+for i in range(len(line)):
+    count += 1
+    if count % 3 == 0:
+        _list.append(line[i])
+print(*_list)
