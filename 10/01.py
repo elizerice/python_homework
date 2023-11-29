36f6613354d087d8e22c1e0cade3bb0b635c7c2a
elizerice <114720693+elizerice@users.noreply.github.com>
2023-11-29 10:15:48 +0500

    Update 01.py

--- 10/01.py
+++ 10/01.py
@@ -1,11 +1,11 @@
-list = []
+_list = []
 yes_no = bool()
 for i in range(int(input())):
     number = input()
-    list.append(number)
+    _list.append(number)
 num = int(input())
-for i in list:
-    for j in list:
+for i in _list:
+    for j in _list:
         if int(i) * int(j) == num:
             yes_no = True
 if yes_no == True:
