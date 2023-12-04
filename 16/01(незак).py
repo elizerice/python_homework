9a72a3468da7cab0d07903f7d9838098cd393e1e
elizerice <114720693+elizerice@users.noreply.github.com>
2023-12-04 23:14:30 +0500

    Create 01(незак).py

---
+++ 16/01(незак).py
@@ -0,0 +1,9 @@
+def circle_length(radius):
+    return 2 * pi * radius
+def circle_area(radius):
+    return pi * radius ** 2
+def main():
+    pi = 3.14
+    print(circle_area(10))
+
+circle_area()
