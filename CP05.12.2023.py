1f69193f878fdd457271cbaf83ee2048a481e5ae
elizerice <114720693+elizerice@users.noreply.github.com>
2023-12-05 11:21:16 +0500

    Update CP05.12.2023.py

--- CP05.12.2023.py
+++ CP05.12.2023.py
@@ -1,1 +1,12 @@
 https://disk.yandex.ru/d/dCRdQi0UaZ8P7Q
+#1незак
+lines = list()
+import sys
+for line in sys.stdin:
+    lines.append((line.strip('\n')).split('-=-'))
+line_filter = []
+
+for line in lines:
+        line_filter.append("&".join(filter(lambda x: len(str(line)) % 2 == 0, line)).split())
+for n in line_filter:
+    print(*n)
