55107e76aeac86589fd07e1bb698089ad6cacbbf
elizerice <114720693+elizerice@users.noreply.github.com>
2023-12-07 22:47:14 +0500

    Create 03.py

---
+++ 16/03.py
@@ -0,0 +1,14 @@
+last_message = ''
+def print_without_duplicates(message):
+    global last_message
+    if message != last_message:
+        print(message)
+    last_message = message
+
+print_without_duplicates("Привет")
+print_without_duplicates("Не могу до тебя дозвониться")
+print_without_duplicates("Не могу до тебя дозвониться")
+print_without_duplicates("Не могу до тебя дозвониться")
+print_without_duplicates("Когда доедешь до дома")
+print_without_duplicates("Ага, жду")
+print_without_duplicates("Ага, жду")
