a6db1843374827ab08676f75db0fb2b45af0dfec
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-06 13:17:02 +0500

    Create 09.py

---
+++ 06/09.py
@@ -0,0 +1,14 @@
+freezer = set()
+yes_recipe = set()
+for i in range(int(input('продуктов в холодильнике:'))):
+    freezer.add(input())
+for i in range(int(input('рецептов:'))):
+    recipe_name = input('имя рецепта:')
+    recipe_food = set()
+    for i in range(int(input('ингридиентов:'))):
+        food = input()
+        recipe_food.add(food)
+    if recipe_food <= freezer:
+        yes_recipe.add(recipe_name)
+for i in yes_recipe:
+        print(i)
