ebac8ae942aef92ffa07d36ae74d924a2bbf41bb
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-06 13:21:20 +0500

    Update 09.py

--- 06/09.py
+++ 06/09.py
@@ -5,7 +5,7 @@ for i in range(int(input('продуктов в холодильнике:'))):
 for i in range(int(input('рецептов:'))):
     recipe_name = input('имя рецепта:')
     recipe_food = set()
-    for i in range(int(input('ингридиентов:'))):
+    for j in range(int(input('ингридиентов:'))):
         food = input()
         recipe_food.add(food)
     if recipe_food <= freezer:
