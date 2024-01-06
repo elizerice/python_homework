a03583033e9b44e6bc23798ba6e149b9085f2a32
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-06 22:44:36 +0500

    Update 01.py

--- 08/01.py
+++ 08/01.py
@@ -1,1 +1,13 @@
-
+word = input()
+max_word = ''
+min_word = word
+while word != 'стоп':
+    word = input()
+    if len(max_word) < len(word):
+        max_word = word
+    if len(min_word) > len(word):
+        min_word = word
+if set(min_word) < set(max_word):
+    print('yes')
+else:
+    print('no')
