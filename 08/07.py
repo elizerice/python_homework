1c630fa4eae2e092be4edab5f96120011d775868
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-08 23:12:03 +0500

    Update 07.py

--- 08/07.py
+++ 08/07.py
@@ -1,8 +1,11 @@
 cat_num = int()
+cat_list = list()
 for i in range(int(input())):
     phrase = str(input())
     if 'кот' in phrase or 'Кот' in phrase:
         for j in range(len(phrase)):
             if phrase[j] == 'к' or phrase[j] == 'К':
                 cat_num = j + 1
-        print(i + 1, cat_num)
+        cat_list.append((i + 1, cat_num))
+for i in cat_list:
+    print(*i)
