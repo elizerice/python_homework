7b43ebaa7abaa529c26f9105cbb86d8328d6c52c
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-11 00:30:58 +0500

    Update 08.py

--- 12/08.py
+++ 12/08.py
@@ -4,12 +4,10 @@ max_symb = ''
 max_count = 0
 count = 1
 for i in range(1, len(line)):
-    if i == line[i-1]:
+    if line[i] == line[i-1]:
         count += 1
     else:
         if count > max_count and line[i-1] != ' ':
             max_count = count
             max_symb = line[i - 1]
-        else:
-            break
 print(max_symb)
