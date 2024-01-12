ff37c5b79d52ee93bd622b7a7a7963a7f1e6a177
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-12 23:35:10 +0500

    Update 10.py

--- 14/10.py
+++ 14/10.py
@@ -1,6 +1,6 @@
 def golden_ratio(i):
     fibonacci = [1, 1]
-    for i in range(2, i + 1):
-        fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+    for j in range(2, i + 1):
+        fibonacci.append(fibonacci[j - 1] + fibonacci[j - 2])
         print(fibonacci)
     print(fibonacci[i] / fibonacci[i - 1])
