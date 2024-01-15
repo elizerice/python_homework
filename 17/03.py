6df002ef37e4a81272070220e09e3f2d38f78a57
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-15 15:42:58 +0500

    Create 03.py

---
+++ 17/03.py
@@ -0,0 +1,8 @@
+example_list = [1, 5, 8, 4, 6, 9, 21, -6]
+print(id(example_list)) #id списка
+
+sort_list = sorted(example_list) # создается новый отсортированный список со своим id
+example_list.sort() # id остается прежним, список сортируется
+
+print(sort_list, id(sort_list)) #sorted(), id
+print(example_list, id(example_list)) #.sort
