e83334fd08b88c576fd442a54e1b0d4cfa8e7e94
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-17 00:14:56 +0500

    Update 06.py

--- 18/06.py
+++ 18/06.py
@@ -1,13 +1,12 @@
 def solve(*coefficients):
-    coefficients = list(coefficients)
-    a,b,c = coefficients[::]
+    a, b, c = coefficients
     if len(coefficients) == 3:
         discriminant = b ** 2 - 4 * a * c
         x1 = (-b + discriminant ** 0.5) / (a * 2)
         x2 = (-b - discriminant ** 0.5) / (a * 2)
         return x1, x2
     elif len(coefficients) == 2:
-        return - b / (2 *a)
+        return -b / (2 * a)
     elif len(coefficients) == 1:
         return 0
     else:
