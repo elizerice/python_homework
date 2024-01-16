5bcd10874f3c9cf482250009f6b4df04a944aa2d
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-17 00:14:14 +0500

    Create 07.py

---
+++ 18/07.py
@@ -0,0 +1,16 @@
+def solve(*coefficients):
+    a, b, c = coefficients
+    if len(coefficients) == 3:
+        discriminant = b ** 2 - 4 * a * c
+        x1 = (-b + discriminant ** 0.5) / (a * 2)
+        x2 = (-b - discriminant ** 0.5) / (a * 2)
+        return x1, x2
+    elif len(coefficients) == 2:
+        return -b / (2 * a)
+    elif len(coefficients) == 1:
+        return 0
+    else:
+        return "None"
+
+
+print(sorted(solve(*list(map(int, input().split())))))
