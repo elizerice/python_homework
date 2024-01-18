ab02491006b38e63b0d28d36a1ccc1abb4a45283
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-18 17:56:59 +0500

    Update 06.py

--- 19/06.py
+++ 19/06.py
@@ -1,10 +1,6 @@
 def find_farthest_orbit(list_of_orbits):
-    list_of_orbits = filter(lambda x: x[0] != x[1], list(list_of_orbits))
-    max_area = 0
-    orbits_list = list()
-    for orbit in list_of_orbits:
-        orbits_list.append((orbit[0], orbit[1]))
-        if max_area < orbit[0] * orbit[1] * 3.14:
-            max_area = orbit[0] * orbit[1] * 3.14
-            max_orbit = (orbit[0], orbit[1])
-    return max_orbit
+    orbits_sq_list= []
+    orbits_list = []
+    [orbits_sq_list.append((orbit[0] * orbit[1])) for orbit in filter(lambda x: x[0] != x[1], list(list_of_orbits)) if orbit[0] != orbit[1]]
+    [orbits_list.append(i) for i in list_of_orbits if sorted(orbits_sq_list, reverse=True)[0] == i[0] * i[1]]
+    return orbits_list
