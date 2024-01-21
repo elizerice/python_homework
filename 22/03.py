91fd0013115322d02835cbad3ba222f16c293dd0
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-21 23:42:38 +0500

    Update 03.py

--- 22/03.py
+++ 22/03.py
@@ -1,9 +1,12 @@
 import string
 import random
+
+
 def generate_password(m):
     symbols = string.ascii_letters + string.digits
     symbols = "".join(symb for symb in symbols if symb not in "Ll1iIOo0")
     return "".join(random.choice(symbols) for _ in range(m))
+
 
 def main(n, m):
     passwords = list()
