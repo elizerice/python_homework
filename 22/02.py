13294ff9e381709504cdf5cad8b348d3d142ec9f
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-21 21:49:35 +0500

    Create 02.py

---
+++ 22/02.py
@@ -0,0 +1,17 @@
+from random import choice
+import sys
+people = []
+for line in sys.stdin:
+    people.append(line.split())
+def secret_friend(friends_list):
+    persons = friends_list.copy()
+    for i in friends_list:
+        rand_pers = choice(persons)
+        while rand_pers == i:
+            rand_pers = choice(persons)
+        else:
+            print(*i, '-', *rand_pers)
+            persons.remove(rand_pers)
+
+
+secret_friend(people)
