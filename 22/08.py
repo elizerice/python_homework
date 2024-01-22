9582cdec57974298d34f6f68a2d08cc81d4b3603
elizerice <114720693+elizerice@users.noreply.github.com>
2024-01-22 17:28:39 +0500

    Create 08.py

---
+++ 22/08.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+
+def generate_password(m):
+    uppercase_letters = string.ascii_uppercase
+    lowercase_letters = string.ascii_lowercase
+    digits = string.digits
+    symbols = uppercase_letters + lowercase_letters + digits
+    no_symbs = "Ll1iIOo0"
+    for symb in no_symbs:
+        symbols = symbols.replace(symb, "")
+    return "".join(random.sample(symbols, m))
+
+
+def main(n, m):
+    passwords = set()
+    while len(passwords) < n:
+        passwords.add(generate_password(m))
+    return list(passwords)
